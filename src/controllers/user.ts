84d7832b2577f4d63c000f0dc7c0cc65b8ba7f29
Ibrahim Faruqi <izf@izfaruqi.com>
2021-03-01 00:25:38 +0700

    Add error handling

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -13,6 +13,15 @@ export async function register(ctx: ParameterizedContext){
     email: ctx.request.body.email,
     password: await bcrypt.hash(ctx.request.body.password, BCRYPT_ROUNDS)
   }
-  const res = await getConnection().getRepository(User).insert(user)
-  ctx.body = { id: res.identifiers[0].id }
+  try {
+    const res = await getConnection().getRepository(User).insert(user)
+    ctx.body = { id: res.identifiers[0].id }
+  } catch (e: any){
+    switch(e.code){
+      case "ER_DUP_ENTRY":
+        ctx.throw(403, e)
+      default:
+        ctx.throw(500, e)
+    }
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,18 @@ import 'reflect-metadata'
 import Koa from 'koa'
 import Helmet from 'koa-helmet'
 import BodyParser from 'koa-bodyparser'
+import BetterErrorHandler from 'koa-better-error-handler'
 import { AddressInfo } from 'net'
 import { router } from './routes'
 import initDB from './db'
-import './entities/User'
 
 export const APP_VERSION = "0.1.0"
 dotenv.config()
 
 const app = new Koa()
 
+app.context.api = true
+app.context.onerror = BetterErrorHandler()
 app.use(Helmet())
 app.use(BodyParser())
 app.use(router.routes())
