ac327be3a356d9cb1dce2c80a37072c1fbe2dcf5
Ibrahim Faruqi <izf@izfaruqi.com>
2021-03-01 22:54:08 +0700

    Use env for db parameters

--- src/db.ts
+++ src/db.ts
@@ -5,11 +5,11 @@ import { User } from './entities/User'
 export default async function initDB() {
   await createConnection({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "barengin",
-    password: "barengin2021",
-    database: "barengin",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT? parseInt(process.env.DB_PORT) : 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     "entities": [
