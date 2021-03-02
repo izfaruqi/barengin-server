955e866fd938a2e545c93fa3afe95d60931abab7
Ibrahim Faruqi <izf@izfaruqi.com>
2021-03-03 01:40:25 +0700

    Add clear all data endpoint for development

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,8 +1,19 @@
-import Router from 'koa-router'
+import Router from 'koa-joi-router'
+import { getConnection } from 'typeorm'
 import { APP_VERSION } from '../index'
 
-export const router = new Router()
+export const router = Router()
 
-router.get("/", ctx => {
+router.get("/", async ctx => {
   ctx.body = { version: APP_VERSION }
+})
+
+router.get("/UNSAFEclearAllData", async ctx => {
+  const entities = getConnection().entityMetadatas;
+
+  for (const entity of entities) {
+      const repository = await getConnection().getRepository(entity.name);
+      await repository.clear();
+  }
+  ctx.body = { success: true }
 })
