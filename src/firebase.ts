9fd9928395ed0458ab1c30eb08a65098011cf467
Ibrahim Faruqi <izf@izfaruqi.com>
2021-03-07 21:54:09 +0700

    Move firebase config to external json with env

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import firebase from 'firebase'
 import admin from 'firebase-admin'
+import path from 'path'
 
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
@@ -7,13 +8,6 @@ export function initFirebase(){
   admin.initializeApp({
     credential: admin.credential.applicationDefault()
   })
-  firebase.initializeApp({
-    apiKey: "AIzaSyDZ1DdW0jFBbWrdkkKqdoY95NI0PvuFdi4",
-    authDomain: "bareng-in.firebaseapp.com",
-    projectId: "bareng-in",
-    storageBucket: "bareng-in.appspot.com",
-    messagingSenderId: "18568775224",
-    appId: "1:18568775224:web:2e8bd23b6f230a5a8588cd",
-    measurementId: "G-85YDMPMGEW"
-  })
+
+  firebase.initializeApp(require(path.join("..", process.env.GOOGLE_FIREBASE_CONFIG!)))
 }
