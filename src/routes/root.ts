76184cb615b12c3db22a482356aa622e7d8b4df0
Ibrahim Faruqi <izf@izfaruqi.com>
2021-03-11 01:13:16 +0700

    Add landing page stats endpoint

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,11 +1,23 @@
 import Router from 'koa-joi-router'
 import { getConnection } from 'typeorm'
+import { Group } from '../entities/Group'
+import { GroupCategory } from '../entities/GroupCategory'
+import { User } from '../entities/User'
 import { APP_VERSION } from '../index'
 
 export const router = Router()
 
+// TODO: Might need to clean this up later.
 router.get("/", async ctx => {
   ctx.body = { version: APP_VERSION }
+})
+
+router.get("/landing-stats", async ctx => {
+  ctx.body = {
+    users: await getConnection().getRepository(User).count(),
+    groups: await getConnection().getRepository(Group).count(),
+    groupCategories: await getConnection().getRepository(GroupCategory).count(),
+  }
 })
 
 router.get("/UNSAFEclearAllData", async ctx => {
